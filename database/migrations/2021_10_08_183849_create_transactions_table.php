fccf9fc8fdd5d263ad222d5560a7b536c394572a
Filipp Uymenov <uymenov.p@drupalsib.ru>
2021-10-09 02:25:15 +0700

    Implements Transaction model with migration and update User migration

---
+++ database/migrations/2021_10_08_183849_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('sender_id');
+            $table->unsignedInteger('recipient_id');
+            $table->unsignedBigInteger('amount');
+            $table->enum('status', ['planned', 'completed'])->default('planned');
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('sender_id')->references('id')->on('users');
+            $table->foreign('recipient_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
